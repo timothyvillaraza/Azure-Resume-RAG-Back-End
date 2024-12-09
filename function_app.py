73c0910ffcdf17e6f2828e7a781f745b61c86caa
timothyvillaraza <61322637+timothyvillaraza@users.noreply.github.com>
2024-12-09 12:08:48 -0600

    Reduced requirements

--- function_app.py
+++ function_app.py
@@ -2,9 +2,6 @@ import azure.functions as func
 from services.resume_rag.api.functions import resume_rag_functions
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
-load_dotenv()
-
 app = func.FunctionApp()
 
 # Register blueprints
